c7f2872ac54341ce062496458d5acc693b20143e
Amir Gamil <amir.gamil.dev@gmail.com>
2021-08-10 11:49:58 +0100

    Initial commit

---
+++ FactorialFromFlowchart/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialFromFlowchart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the value for n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int fact = Factorial(n);
+
+            Console.WriteLine($"Factorial of {n} is {fact}");
+        }
+
+        public static int Factorial(int n)
+        {
+            int fact = 1;
+
+            while(n > 1)
+            {
+                fact *= n;  // same as fact = fact * n
+                n--;
+            }
+
+            return fact;
+        }
+    }
+}
