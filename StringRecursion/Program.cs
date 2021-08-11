1177eeaee4f9880ba26b9d3855185e6d3461a498
Amir Gamil <amir.gamil.dev@gmail.com>
2021-08-11 14:44:04 +0100

    Add StringRecursion exercise solution

---
+++ StringRecursion/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter word or phrase");
+            string s = Console.ReadLine();
+
+            Console.WriteLine("\nIterative\nOutput:");
+            DoIterativeStringEcho(s);
+
+            Console.WriteLine("\nRecursive\nOutput:");
+            DoRecursiveStringEcho(s);
+        }
+
+        private static void DoIterativeStringEcho(string s)
+        {
+            for(int i = s.Length; i > 0; i--)
+            {
+                Console.WriteLine(s.Substring(0, i));
+            }
+        }
+
+        private static void DoRecursiveStringEcho(string s)
+        {
+            Console.WriteLine(s);
+
+            if (s.Length == 1)
+            {
+                return;
+            }
+            else
+            {
+                string substring = s.Substring(0, s.Length - 1);
+                DoRecursiveStringEcho(substring);
+            }
+        }
+    }
+}
