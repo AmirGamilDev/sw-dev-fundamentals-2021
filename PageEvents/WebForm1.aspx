d3c1f1e865ac3e04e6bc4cc705a6e03ca98b4d73
Amir Gamil <amir.gamil.dev@gmail.com>
2021-08-17 11:51:20 +0100

    Add PageEvents example

---
+++ PageEvents/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="PageEvents.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    
+    <p><em>Page being loaded at <% = DateTime.Now.ToLocalTime() %></em></p>
+
+    <form id="form1" runat="server">
+        <div>                        
+        </div>
+
+        <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblGreeting" runat="server" Text=""></asp:Label>
+        <br />
+        <asp:Button ID="btnSayHello" runat="server" Text="Say Hello!" OnClick="btnSayHello_Click" />
+    </form>
+</body>
+</html>
---
+++ PageEvents/WebForm1.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace PageEvents
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string now = DateTime.Now.ToLocalTime().ToString();
+
+            Response.Write($"<p>Message from Page_Load at {now}</p>");
+        }
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            string now = DateTime.Now.ToLocalTime().ToString();
+
+            Response.Write($"<p>Message from Page_Init at {now}</p>");
+        }
+
+        protected void Page_DataBind(object sender, EventArgs e)
+        {
+            Response.Write("<p>Message from Page_DataBind.<p>");
+        }
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            System.Console.WriteLine("In Page_Unload");
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            string now = DateTime.Now.ToLocalTime().ToString();
+
+            Response.Write($"<p>Message from Page_PreRender at {now}</p>");
+        }
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            string now = DateTime.Now.ToLocalTime().ToString();
+
+            Response.Write($"<p>Message from Page_PreInit at {now}</p>");
+        }
+
+        protected void btnSayHello_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+
+            if(name.Length > 0)
+            {
+                lblGreeting.Text = $"Hello {name}!";
+            }
+            else
+            {
+                lblGreeting.Text = "Hello world!";
+            }
+
+        }
+    }
+}
