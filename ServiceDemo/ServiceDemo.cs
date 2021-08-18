b62fa6c249d5ad801447cfaac82a2bd0797b32a3
Amir Gamil <amir.gamil.dev@gmail.com>
2021-08-18 11:39:48 +0100

    Add ServiceDemo example

---
+++ ServiceDemo/ServiceDemo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceDemo
+{
+    public partial class ServiceDemo : ServiceBase
+    {
+        public ServiceDemo()
+        {
+            InitializeComponent();
+            if (!EventLog.SourceExists("ServiceDemo"))
+            {
+                EventLog.CreateEventSource("ServiceDemo", "Application");
+            }
+            eventLog1.Source = "ServiceDemo";
+            eventLog1.Log = "Application";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("Starting the service", EventLogEntryType.Information, 1001);
+        }
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("Stopping the service",EventLogEntryType.Information, 1001);
+        }
+
+        protected override void OnPause()
+        {
+            eventLog1.WriteEntry("Pausing the service",EventLogEntryType.Information, 1001);
+        }
+        protected override void OnContinue()
+        {
+            eventLog1.WriteEntry("Continuing the service",EventLogEntryType.Information, 1001);
+        }
+        protected override void OnShutdown()
+        {
+            eventLog1.WriteEntry("Shutting down the service",EventLogEntryType.Information, 1001);
+        }
+    }
+}
